a7cdd56a51423095eb0ae0da782cda8d3bae8ffd
Mavzuna Khairullo <mavzuna_24@mail.ru>
2022-04-13 18:34:31 +0500

    fixed all
    изменила все

--- MyFirstApp/MainBoardViewController.swift
+++ MyFirstApp/MainBoardViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class MainBoardViewController: UIViewController {
-   
+    
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var guessNumberLabel: UILabel!
@@ -36,7 +36,6 @@ class MainBoardViewController: UIViewController {
     }
     
     @IBAction func didTapSelectButton(_ sender: Any) {
-        
         print("Значение слайдера: ", Int(slider.value))
         //1. забрать число у слайдера
         let sliderValue: Float = slider.value
@@ -55,11 +54,12 @@ class MainBoardViewController: UIViewController {
         }
         updateGuessingNumber()
         //
-}
+    }
+    
     //настройка игры
     func setUp( ) {
         //установим начальное значение слайдера
-        slider.value = 21
+        slider.value = 50
         //конкатенируем текст и рандомное число
         updateGuessingNumber()
         //конкатенируем текст и количество очков
@@ -67,15 +67,52 @@ class MainBoardViewController: UIViewController {
         scoreLabel.text = "Очки: \(score)"
         //количество оставшихся попыток
         remainAttempt = 1
-        remainingAttempt.text = "Раунд: \(remainAttempt)"
+        remainingAttempt.text = "Раунд:" + String(remainAttempt)
+        if remainAttempt == 11 {
+            setUp()
+        }
+        
     }
     
     func updateGuessingNumber() {
         guessNumber = Int.random(in: 1...100)
         guessNumberLabel.text = ("Попробуйте угадать число: \(guessNumber)")
     }
+    
+    func setScore() {
+        if guessNumber == Int(slider.value) {
+            score = score + 10
+            scoreLabel.text  = "Scores:" + String(score)
+        }
+        
+    }
+    
     func remAttempt() {
         remainAttempt += 1
-        remainingAttempt.text = "Раунд: \(remainAttempt)"
+        remainingAttempt.text = "Раунд:" + String(remainAttempt)
+        if remainAttempt == 11 {
+            setUp()
+        }
     }
+    
+    func showResults() {
+        //creat alert
+        let alert: UIAlertController = UIAlertController (title: "Results", message: "You earned\(score)scores" , preferredStyle: .alert)
+        //creat a button for alert
+        let okButton: UIAlertAction = UIAlertAction(title: "Start again", style: .default, handler: { _ in
+            // creat action when button is pressed
+            print ("You pressed the button")
+            
+            self.setUp()
+        })
+        
+        //add a button for alert
+        alert.addAction(okButton)
+        //display th ealert on the screen
+        present(alert, animated: true)
+    }
+    
 }
+
+
+
